5617510b0602c217de7600504674f29082c18486
Chen Wenbo <chen.wenbo@jp.panasonic.com>
2020-10-12 13:06:54 +0900

    add No.29 No.125 No.155

---
+++ 125.valid-palindrome.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=125 lang=cpp
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (37.19%)
+ * Likes:    1465
+ * Dislikes: 3212
+ * Total Accepted:    697.8K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases.
+ * 
+ * Note: For the purpose of this problem, we define empty string as valid
+ * palindrome.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "race a car"
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * s consists only of printable ASCII characters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    }
+};
+// @lc code=end
+
---
+++ 155.min-stack.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=155 lang=cpp
+ *
+ * [155] Min Stack
+ *
+ * https://leetcode.com/problems/min-stack/description/
+ *
+ * algorithms
+ * Easy (45.02%)
+ * Likes:    3929
+ * Dislikes: 370
+ * Total Accepted:    608.9K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
+ *
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * 
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ * 
+ * Explanation
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin(); // return -3
+ * minStack.pop();
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * Methods pop, top and getMin operations will always be called on non-empty
+ * stacks.
+ * 
+ * 
+ */
+
+// @lc code=start
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        
+    }
+    
+    void pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    int getMin() {
+        
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+// @lc code=end
+
